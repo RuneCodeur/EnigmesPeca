6f327bec90d749b672598404875c664d60e14b3f
Jonathan <yreelfactory@gmail.com>
2024-08-25 21:43:36 +0200

    ajustement grand jeu 2

--- enigmes/GJ2-accueil.php
+++ enigmes/GJ2-accueil.php
@@ -20,6 +20,9 @@ if(isset($_GET['quit'])){
     die;
 }
 
+if(isset($_SESSION['equipe'])){
+    testTeam($fichierTeam, "GJ2-accueil");
+}
 
 if (isset($_POST['selectEquip']) && $_POST['selectEquip'] !='') {
     
@@ -28,6 +31,25 @@ if (isset($_POST['selectEquip']) && $_POST['selectEquip'] !='') {
     }
     else{
         $resultat = "Nom d'equipe déja pris !";
+    }
+}
+
+// création d'une equipe
+if (isset($_POST['newEquip']) && $_POST['newEquip'] !='') {
+    
+    if (preg_match('/[^a-zA-Z0-9-]/', $_POST['newEquip'])) {
+        $resultat = "caractères non autorisé !";
+    }
+    elseif(isset($equipes[strtolower($_POST['newEquip'])])){
+        $resultat = "Nom d'equipe déja pris !";
+    }
+    else{
+        $equipes[$_POST['newEquip']] = [
+            'score' => 0,
+            'liste' => [],
+        ];
+        UPDATEfichier($fichierTeam, $equipes);
+        $_SESSION['equipe'] = $_POST['newEquip'];
     }
 }
 
@@ -44,6 +66,10 @@ if(isset($_SESSION['equipe'])){
 <?php
 if($afficheFormulaire){
     ?>
+    <p class="resultat <?=$classResultat?>">
+        <?= $resultat ?>
+    </p>
+    
 
     <p> Avant de jouer, choisissez votre équipe !</p>
 
@@ -60,6 +86,15 @@ if($afficheFormulaire){
         </select>
 
         <input class="bout_action_stylé" type="submit">
+    </form>
+
+    <br>
+    
+    <p> Ou alors, ajoute ton nom d'équipe !</p>
+
+    <form style=" min-width:190px;" class="formulaire_stylé" action="<?=lienEnigme($nomEpreuve)?>" method="POST">
+        <input class="bout_stylé" type="text" name="newEquip" placeholder="nom d'équipe" required>
+        <input class="bout_action_stylé" type="submit" value="création">
     </form>
 
     <?php 
